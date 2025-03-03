f2ce3c06de1c60815e93558c44e7df9febc20707
soubhagyabitt123 <58928384+soubhagyabitt123@users.noreply.github.com>
2025-03-03 11:40:16 +0100

    Update MortgageServiceApplication.java

--- src/main/java/com/mortgage/service/app/MortgageServiceApplication.java
+++ src/main/java/com/mortgage/service/app/MortgageServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Employee Management API", version = "v1", description = "API for managing employees, projects and roles"))
 public class MortgageServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MortgageServiceApplication.class, args);
